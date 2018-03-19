460629324fe8c82a48f632f7ef2a39464de76bd2
Chris Douillard <cdouillard@me.com>
2018-03-19 17:57:00 -0600

    Decided to commit the .env file. Removed hardcoded settings from model file.

--- api/models/track.js
+++ api/models/track.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const config = {
-    host:'mariadb',
-    user: 'root',
-    password: 'password',
-    database: 'Chinook'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 }
 
 exports.getAll = async function () {
